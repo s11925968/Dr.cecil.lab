e07f24d3245844ac68734fed8ee6293cafaee209
sameh issa <s11925968@gmail.com>
2024-04-28 09:50:52 +0300

    fixed cors

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import initApp from "./src/app.router.js";
-
 const app = express();
 const PORT = process.env.PORT || 3000; 
 initApp(app, express);
--- src/app.router.js
+++ src/app.router.js
@@ -1,11 +1,12 @@
 import connectDb from "../DB/connection.js";
 import AuthRouter from "./module/auth/auth.router.js";
+import cors from 'cors'
 
 const initApp = async (app, express) => {
     app.use(express.json());
     connectDb();
     app.use("/auth", AuthRouter);
-
+    app.use(cors())
     app.use("/*", (req, res) => {
         return res.json({ message: "Page Not Found" });
       });
