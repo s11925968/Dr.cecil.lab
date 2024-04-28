d6b149e2ff3fdde5d1b05952755b3e7bdc850126
sameh issa <s11925968@gmail.com>
2024-04-28 10:02:59 +0300

    fixed cors

--- src/app.router.js
+++ src/app.router.js
@@ -1,14 +1,15 @@
 import connectDb from "../DB/connection.js";
 import AuthRouter from "./module/auth/auth.router.js";
-import cors from 'cors'
+import cors from 'cors';
 
 const initApp = async (app, express) => {
-    app.use(express.json());
-    connectDb();
-    app.use("/auth", AuthRouter);
-    app.use(cors())
-    app.use("/*", (req, res) => {
-        return res.json({ message: "Page Not Found" });
-      });
+  app.use(cors()); // Enable CORS
+  app.use(express.json());
+  connectDb();
+  app.use("/auth", AuthRouter);
+  app.use("/*", (req, res) => {
+      return res.json({ message: "Page Not Found" });
+  });
 };
+
 export default initApp;
